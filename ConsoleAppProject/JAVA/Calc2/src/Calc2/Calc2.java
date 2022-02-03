f31adbff81278d6f9faca3857e5545345d1b853f
Hanan Butt <100150264@student.burnley.ac.uk>
2022-02-03 15:48:34 +0000

    java update 2

---
+++ ConsoleAppProject/JAVA/Calc2/src/Calc2/Calc2.java
@@ -0,0 +1,39 @@
+package Calc2;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 100150264
+ */
+public class Calc2 {
+   String name; 
+   
+   public int add(int x, int y){
+       return x + y; 
+   
+       
+   }
+   public int subtract(int x, int y){
+       
+       return x - y; 
+   }
+   
+   
+   public int multiply(int x, int y){
+       return x * y; 
+   }
+   
+   
+   public int divide(int x, int y){
+       return x / y; 
+   }
+   
+   
+   public int modulus(int x, int y){
+       return x % y;    // 5 % 4 = 1 (modulus operator is a precent sign that finds the remainder when a number is divided) 
+   } 
+}
---
+++ ConsoleAppProject/JAVA/Calc2/src/Calc2/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Calc2;
+
+/**
+ *
+ * @author 100150264
+ */
+public class Main {
+   
+    public static void main(String[] args)
+    {
+        Calc2 c1= new Calc2();
+        Calc2 c2= new Calc2();
+        
+        c1.name = "first calculator";
+        c2.name = "second calculator";
+        
+        
+        System.out.println(c1.name + " calculated " + c1.add(3, 4));
+        System.out.println(c2.name + " calculated " + c2.modulus(29, 5));
+    }  
+}
